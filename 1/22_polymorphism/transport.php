7053730de2354446d65282ccd97196c3db474521
Roman <kotelnikovrm@gmail.com>
2020-07-19 06:33:58 +0800

    add class Transport in 1/22_polimorphism

--- 1/22_polymorphism/transport.php
+++ 1/22_polymorphism/transport.php
@@ -1,5 +1,20 @@
 <?php
 
-class Transport {}
+class Transport {
+    //вид транспорта
+    //воздушный, наземный, водный
+    public $mode_of_transport;
+
+    //автомобиль, грузовик и т.д.
+    public $category;
+    public $name;
+    public $speed;
+
+    //вид двигателя
+    public $engine_type;
+    
+    //объём двигателя
+    public $engine_volume;
+}
 
 ?>
