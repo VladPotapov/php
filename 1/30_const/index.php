8aad88d95cc13a7af66226f9746c76d03ad08182
Roman <kotelnikovrm@gmail.com>
2020-08-06 12:13:32 +0800

    разобрался со статическими методами и начал осваивать константы

--- 1/30_const/index.php
+++ 1/30_const/index.php
@@ -1,7 +1,7 @@
 <?php
 
 class Cls {
-    const NAME = "Cls";
+    const NAME = "Cls <br>";
 
     public function method() {
         //error
@@ -9,7 +9,17 @@ class Cls {
 
         echo self::NAME;
         echo "<br>";
+        echo cls::NAME;
+        echo "<br>";
     }
 }
+
+echo cls::NAME;
+
+$obj = new Cls();
+$obj->method();
+
+if(defined("Cls::NAME")) echo " константа определена <br>";
+else echo " константа не определена <br>";
 
 ?>
