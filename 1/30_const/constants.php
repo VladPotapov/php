59aa6507cd5f01a9d9a47fafeb8e27ca83329bbd
Roman <kotelnikovrm@gmail.com>
2020-08-23 11:08:11 +0800

    освоил константы

---
+++ 1/30_const/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+class constants {
+    const MAX_VALUE = 10;
+    //error так как константа не может быть публичной
+    //public const MIN_VALUE = 1;
+}
+
+echo constants::MAX_VALUE;
+
+?>
